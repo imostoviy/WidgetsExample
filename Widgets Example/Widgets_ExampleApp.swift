d37a0fb36c571a68622ca90ac9b587153e8bf553
Ihor Mostovyi <imostovyi@malwarebytes.com>
2022-07-24 18:03:55 +0300

    Initial Commit

---
+++ Widgets Example/Widgets_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widgets_ExampleApp.swift
+//  Widgets Example
+//
+//  Created by Ihor Mostovyi on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Widgets_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
